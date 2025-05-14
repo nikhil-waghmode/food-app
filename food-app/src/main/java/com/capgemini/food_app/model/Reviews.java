b1a0e304f01ba4cd1dd07f91e8c07451472122f4
Tejas Lamkhade <105742352+Tejas-24ytj@users.noreply.github.com>
2025-05-14 15:24:31 +0530

    Update Reviews.java
    Fixed @Id

--- food-app/src/main/java/com/capgemini/food_app/model/Reviews.java
+++ food-app/src/main/java/com/capgemini/food_app/model/Reviews.java
@@ -13,7 +13,7 @@ import jakarta.persistence.ManyToOne;
 
 public class Reviews {
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JsonBackReference
