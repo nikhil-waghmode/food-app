051610696c2bed9290c1cf5dfc443b765c4b43e9
Bhairavnath-Bhuse <bhairavnathbhuse@gmail.com>
2025-05-14 17:04:30 +0530

    Added FoodItem repository, service, and controller classes

---
+++ food-app/src/main/java/com/capgemini/food_app/service/FoodItemService.java
@@ -0,0 +1,24 @@
+package com.capgemini.food_app.service;
+
+import java.util.List;
+
+import com.capgemini.food_app.model.FoodItem;
+
+public interface FoodItemService {
+	
+	List<FoodItem> getAllFoodItems();
+	
+	FoodItem getFoodItemById(Long id);
+	
+	FoodItem createFoodItem(FoodItem foodItem);
+	
+	FoodItem updateFoodItem(Long id, FoodItem foodItem);
+	
+	FoodItem patchFoodItem(Long id, FoodItem foodItem);
+	
+	void deleteFoodItem(Long id);
+	
+	List<FoodItem> getFoodItemsForRestaurant(Long restaurantID);
+
+	
+}
