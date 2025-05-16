278b4e42f9ff68c59a8382bfd4875ccad921706b
Rahul <rahulpc.3203@gmail.com>
2025-05-16 12:56:23 +0530

    Food Item Related Back-End changes made, manage-food-items.html completed

--- food-app/src/main/java/com/capgemini/food_app/repository/FoodItemRepository.java
+++ food-app/src/main/java/com/capgemini/food_app/repository/FoodItemRepository.java
@@ -5,11 +5,19 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.capgemini.food_app.model.FoodItem;
 
-public interface FoodItemRepository extends JpaRepository<FoodItem, Long>{
+@Repository
+public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
 
-	@Query("SELECT f FROM FoodItem f WHERE f.restaurant.id = :restaurantId")
+    @Query("SELECT f FROM FoodItem f WHERE f.restaurant.id = :restaurantId")
     List<FoodItem> findAllByRestaurantId(@Param("restaurantId") Long restaurantId);
+    
+    List<FoodItem> findByNameContainingIgnoreCase(String name);
+    
+    List<FoodItem> findByCuisineIgnoreCase(String cuisine);
+    
+    List<FoodItem> findByCategoryIgnoreCase(String category);
 }
