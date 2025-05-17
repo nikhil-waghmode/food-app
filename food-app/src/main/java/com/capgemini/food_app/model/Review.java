831b1585052f2a9e097d2dbc077cc28e64617d6a
RishiDeore <deorerishiraj2003@gmail.com>
2025-05-17 12:01:22 +0530

    Rishi Backup Before Merge.

--- food-app/src/main/java/com/capgemini/food_app/model/Review.java
+++ food-app/src/main/java/com/capgemini/food_app/model/Review.java
@@ -17,9 +17,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
-
-
-
 @Entity
 @Table(name="reviews")
 public class Review {
--- food-app/src/main/java/com/capgemini/food_app/service/UserService.java
+++ food-app/src/main/java/com/capgemini/food_app/service/UserService.java
@@ -1,20 +1,28 @@
 package com.capgemini.food_app.service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.capgemini.food_app.model.User;
 
 public interface UserService {
-	User createUser(User user);
-	
-	User updateUser(Long id, User user);
-	
+
+	public User updateUser(Long id, String name, String email, String password, String phone, String location,
+			String userType, MultipartFile profileImage) throws IOException;
+
 	boolean deleteUser(Long id);
-	
+
 	List<User> getAllUsers();
-	
+
 	User getUserById(Long id);
-	
-	User patchUser(Long id,User user);
+
+	public User patchUser(Long id, String name, String email, String password, String phone, String location,
+			String userType, MultipartFile profileImage) throws IOException;
+
+	public User createUser(String name, String email, String password, String phone, String location, String userType,
+			MultipartFile profileImage) throws IOException;
+
 }
